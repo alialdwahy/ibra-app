d246dd04a736d6d0c9090ddecf33e3df9ed6c749
alialdwahy <aligowi358@gmail.com>
2023-08-16 16:10:47 +0300

    brand

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const productCategoryRouter = require("./routes/productCategoryRoute")
 const blogCategoryRouter = require("./routes/blogCategoryRoute");
+const brandRouter = require("./routes/brandRoute")
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
@@ -24,6 +25,7 @@ app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/product-category", productCategoryRouter);
 app.use("/api/blog-category", blogCategoryRouter);
+app.use("/api/brand", brandRouter)
  
 app.use(notFound);
 app.use(errorHandler);
---
+++ routes/brandRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {authMiddleware} = require("../middlewares/authmiddleware");
+const { createBrand, updateBrand, deleteBrand, getBrand, getAllBrand } = require("../controller/brandController");
+const router = express.Router();
+
+
+router.post("/create-brand", authMiddleware, createBrand);
+router.put("/update-brand/:id", authMiddleware, updateBrand);
+router.delete("/delete-brand/:id", authMiddleware, deleteBrand);
+router.get("/get-brand/:id", authMiddleware, getBrand);
+router.get("/getall-brand", authMiddleware, getAllBrand);
+
+
+module.exports = router;
